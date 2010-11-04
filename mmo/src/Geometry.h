5eeb25b622a6a30e9604ec330d550587fbe44fa0
Bill <whess@calpoly.edu>
2010-11-04 01:10:09 -0700

    Geometry done. Untested.

--- mmo/src/Geometry.h
+++ mmo/src/Geometry.h
@@ -2,58 +2,70 @@
 #define _GEOMETRY_H_
 
 #include "matrix.h"
+#include <boost/shared_ptr.hpp>
+#include <vector>
 
 namespace geom {
-
    using namespace mat;
+   using namespace std;
 
    struct Circle;
-   struct Square;
    struct Plane;
 
    //May want to know where they collided?
-   struct Geometry {
-      virtual bool collision(Geometry *other) =0;
-
-      virtual bool collision(Circle *other) = 0;
-      virtual bool collision(Square *other) = 0;
-      virtual bool collision(Plane *other) = 0;
+   struct GeometryBase {
+      virtual bool collision(const GeometryBase *other) const =0;
+      virtual bool collision(const Circle *other) const = 0;
+      virtual bool collision(const Plane *other) const = 0;
+      virtual GeometryBase *clone() const = 0;
    };
 
-   struct Circle : Geometry {
-      Circle(float radius=0, vec2 pos = vec2()) : radius(radius), pos(pos) {}
-      void init(vec2 pos, float radius);
-      bool collision(Geometry *other);
-      bool collision(Circle *other);
-      bool collision(Square *other);
-      bool collision(Plane *other);
+   struct Circle : public GeometryBase {
+      Circle(vec2 pos, float radius) : pos(pos), radius(radius) {}
+      bool collision(const GeometryBase *other) const;
+      bool collision(const Circle *other) const;
+      bool collision(const Plane *other) const;
+      virtual GeometryBase *clone() const 
+         { return new Circle(*this); }
 
+      vec2 pos;
       float radius;
-      vec2 pos;
    };
 
-   struct Square : Geometry {
-      Square(vec2 bottomLeft, vec2 size) : bottomLeft(bottomLeft), size(size) {}
-      void init(vec2 bottomleft, vec2 size);
-      bool collision(Geometry *other);
-      bool collision(Circle *other);
-      bool collision(Square *other);
-      bool collision(Plane *other);
+   struct Plane : GeometryBase {
+      Plane(vec2 v1, vec2 v2, float radius) : radius(radius) {
+         vec2 temp = normalize(to(v1, v2));
+         norm = vec2(-temp.y, -temp.x);
+         d = -norm.dot(v1);
+      }
+      bool collision(const GeometryBase *other) const;
+      bool collision(const Circle *other) const;
+      bool collision(const Plane *other) const;
+      virtual GeometryBase *clone() const
+         { return new Plane(*this); }
 
-      vec2 bottomLeft, size;
+      vec2 norm;
+      float radius, d;
    };
 
-   struct Plane : Geometry {
-      Plane(vec2 pos = vec2(0,0), float slope=0) : pos(pos), slope(slope) {}
-      void init(vec2 pos, float slope);
-      bool collision(Geometry *other);
-      bool collision(Circle *other);
-      bool collision(Square *other);
-      bool collision(Plane *other);
-
-      vec2 pos;
-      float slope; //2d is Y = mX+b
+   struct Geometry {
+      Geometry(GeometryBase &g) : ptr(g.clone()) {}
+      Geometry(GeometryBase *g) : ptr(g) {}
+      bool collision(Geometry g) { return ptr->collision(g.ptr.get()); }
+      boost::shared_ptr<GeometryBase> ptr;
    };
+   
+   struct GeometrySet : GeometryBase {
+      GeometrySet() {}
+      void add(Geometry g) { set.push_back(g); }
+      bool collision(const GeometryBase *other) const;
+      bool collision(const Circle *other) const;
+      bool collision(const Plane *other) const;
+      virtual GeometryBase *clone() const 
+         { return new GeometrySet(*this); }
+      vector<Geometry> set;
+   };
+
 }
 
 #endif //_GEOMETRY_H_
