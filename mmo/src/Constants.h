e0e0c5bdc74c1a4d579af0a4cbbbabbfcdcd0b7a
U-bill-laptop\bill <bill@bill-laptop.(none)>
2010-11-30 03:11:32 -0800

    Made AOI smaller

--- mmo/src/Constants.h
+++ mmo/src/Constants.h
@@ -12,7 +12,7 @@ namespace constants {
 
    const float maxProjectileTicks = 500;
    const float projectileSpeed = 800;
-   const float areaOfInfluenceRadius = 1500;
+   const float areaOfInfluenceRadius = 800;
 
    const int predictTicks = 200;
    const int deltaRegion = 1;
